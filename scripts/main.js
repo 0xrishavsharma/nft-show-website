1f5615359134248bd3a9c342c99cda9dea50ecb3
Rishav Sharma <rishabhtonic2000@gmail.com>
2022-09-09 14:58:26 +0530

    Landing page complete

--- scripts/main.js
+++ scripts/main.js
@@ -25,8 +25,34 @@ navLink.forEach((e) => {
 // Change the background of header
 const onScrollHeader = (e) => {
     let header = document.querySelector(".header");
-    console.log(header)
     this.scrollY >= 50 ? header.classList.add("onScrollHeader") : header.classList.remove("onScrollHeader");
 }
 window.addEventListener('scroll', onScrollHeader)
 
+
+// Countdown timer
+const x = setInterval(() => {
+
+
+    let tillDate = new Date("Nov 1, 2022 23:59:59").getTime();
+    let currentDate = new Date().getTime();
+    let difference = tillDate - currentDate;
+
+    let days = Math.floor(difference / (1000 * 60 * 60 * 24));
+    let hours = Math.floor(difference % (1000 * 60 * 60 * 24) / (1000 * 60 * 60))
+    let minutes = Math.floor(difference % (1000 * 60 * 60) / (1000 * 60))
+    let seconds = Math.floor(difference % (1000 * 60) / (1000))
+
+
+    const timeLeft = {
+        days: days,
+        hours: hours,
+        minutes: minutes,
+        seconds: seconds
+    }
+
+    document.querySelector("#days").innerHTML = days;
+    document.querySelector("#hours").innerHTML = hours;
+    document.querySelector("#minutes").innerHTML = minutes;
+    document.querySelector("#seconds").innerHTML = seconds;
+}, 1000);
